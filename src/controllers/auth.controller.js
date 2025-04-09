5b196ea1cb757d432165dcb124c84fbe46f2e0bb
Gaurav Shrivastava <gauravkrbelsand@gmail.com>
2025-04-09 14:38:05 +0530

    s commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
-  const { fullName, email, password } = req.body;
+  const { fullName, email,password } = req.body;
   try {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
